b05352c5d11931cedbff1d833c4c6730f5f49963
Alec M <apmaster505@gmail.com>
2023-09-09 21:28:20 -0700

    :art: Change About Pic and Animations
    Also add copy security to prevent users from copying the image and stealing it for theft. Additionally, added B&W animation on hover of the image.

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -52,32 +52,51 @@ export default function About() {
         <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
           <div className="lg:pl-20">
             <motion.div
-                animate={{
-                  rotateZ: 3
-                }}
-                whileHover={{
-                  scale: 0.95,
-                  rotateZ: 0,
-                  borderRadius: 15,
-                }}
-                transition={{
-                    duration: 0.5,
-                    type: 'spring',
-                    stiffness: 300
-                }}
-                className="max-w-xs px-2.5 lg:max-w-none"
+              animate={{
+                rotateZ: 3,
+                filter: 'grayscale(1) contrast(1.1)',
+                transition: {
+                  duration: 1,
+                },
+              }}
+              whileHover={{
+                scale: 0.95,
+                rotateZ: 0,
+                borderRadius: 15,
+                filter: 'grayscale(0) contrast(1)',
+                transition: {
+                  duration: 1,
+                },
+              }}
+              transition={{
+                type: 'spring',
+                delay: 0.5,
+                transition: {
+                  ease: 'linear',
+                  duration: 1,
+                },
+              }}
+              className="max-w-xs px-2.5 lg:max-w-none"
             >
               <Image
                 src={portraitImage}
                 alt=""
+                style={{
+                  pointerEvents: 'none',
+                }}
                 sizes="(min-width: 1024px) 32rem, 20rem"
-                className="aspect-square shadow-md rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+                oncontextmenu="return false;"
+                className="aspect-rect rounded-2xl bg-zinc-100 object-cover shadow-md dark:bg-zinc-800"
               />
             </motion.div>
           </div>
           <div className="lg:order-first lg:row-span-2 ">
-            <motion.h1 style={{ fontFamily: 'Layer' }} className="text-5xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 md:text-4xl">
-              I’m <motion.span
+            <motion.h1
+              style={{ fontFamily: 'Layer' }}
+              className="text-5xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 md:text-4xl"
+            >
+              I’m{' '}
+              <motion.span
                 initial={{
                   opacity: 0,
                   scale: 0,
@@ -87,67 +106,84 @@ export default function About() {
                   opacity: 1,
                 }}
                 transition={{
-                  duration: 0.5
+                  duration: 0.5,
                 }}
-                className='bg-gradient-to-r from-teal-600 to-green-400 inline-block text-transparent bg-clip-text'
-            >
-              Alec Mingione
-            </motion.span>. I live in <motion.span
+                className="inline-block bg-gradient-to-r from-teal-600 to-green-400 bg-clip-text text-transparent"
+              >
+                Alec Mingione
+              </motion.span>
+              . I live in{' '}
+              <motion.span
                 initial={{
                   opacity: 0,
                   scale: 0,
                 }}
                 animate={{
                   scale: 1,
-                  opacity: 1
+                  opacity: 1,
                 }}
                 transition={{
                   duration: 0.5,
-                  delay: 0.5
+                  delay: 0.5,
                 }}
-                className='bg-gradient-to-r from-red-600 to-yellow-400 inline-block text-transparent bg-clip-text'
-            >Phoenix Arizona</motion.span>, where I create the&nbsp;
-                <motion.span
-                    initial={{
-                      opacity: 0,
-                      scale: 0,
-                    }}
-                    animate={{
-                      scale: 1,
-                      opacity: 1,
-                      color: 'gray',
-                      textDecoration: '2px teal strikethrough'
-                    }}
-                    transition={{
-                      duration: 0.5,
-                      delay: 1
-                    }}
-                >future</motion.span> to technology.
+                className="inline-block bg-gradient-to-r from-red-600 to-yellow-400 bg-clip-text text-transparent"
+              >
+                Phoenix Arizona
+              </motion.span>
+              , where I create the&nbsp;
+              <motion.span
+                initial={{
+                  opacity: 0,
+                  scale: 0,
+                }}
+                animate={{
+                  scale: 1,
+                  opacity: 1,
+                  color: 'gray',
+                  textDecoration: '2px teal strikethrough',
+                }}
+                transition={{
+                  duration: 0.5,
+                  delay: 1,
+                }}
+              >
+                future
+              </motion.span>{' '}
+              to technology.
             </motion.h1>
-            <div style={{  fontSize: 18 }} className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+            <div
+              style={{ fontSize: 18 }}
+              className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400"
+            >
               <p>
-                I’ve loved making things; being creative for as long as I can remember, and
-                wrote my first program when I was 8 years old, just two weeks
-                after my mom brought home the brand new Emachine 2000 that I
-                taught myself to type on.
+                I’ve loved making things; being creative for as long as I can
+                remember, and wrote my first program when I was 8 years old,
+                just two weeks after my mom brought home the brand new Emachine
+                2000 that I taught myself to type on.
               </p>
               <p>
-                The only thing I loved more than computers as a kid was cars, art, and taking things apart.
-                At 13 I began building computers for the time trying to create the biggest and baddest gaming PC.
-                My first build was an ASUS with 4GBs of DDR2 RAM. A caveman that thing is these days.
+                The only thing I loved more than computers as a kid was cars,
+                art, and taking things apart. At 13 I began building computers
+                for the time trying to create the biggest and baddest gaming PC.
+                My first build was an ASUS with 4GBs of DDR2 RAM. A caveman that
+                thing is these days.
               </p>
               <p>
-                Taking classes and becoming more aware with the advancements in technology. At 15 I had been taking a computer class
-                that only a few kids got allowed to take. Excited, I learned about HTML, CSS, and JS along with how it
-                works on the computer itself going down to assembly code.
-                I had built a Facebook clone single page showcasing myself, my future career, and why.
-                It was so hard, but once I figured it out I wanted it to be the best thing in the class.
+                Taking classes and becoming more aware with the advancements in
+                technology. At 15 I had been taking a computer class that only a
+                few kids got allowed to take. Excited, I learned about HTML,
+                CSS, and JS along with how it works on the computer itself going
+                down to assembly code. I had built a Facebook clone single page
+                showcasing myself, my future career, and why. It was so hard,
+                but once I figured it out I wanted it to be the best thing in
+                the class.
               </p>
               <p>
-                Today, I’m the founder of MiPi, where we’re working on
-                citizen privacy, encryption, free speech, and blockchain technology to empower the people at
-                home so that the next generation of kids can really <em>understand</em>{' '}
-                privacy from their hands defining true freedom and controlled exposure.
+                Today, I’m the founder of MiPi, where we’re working on citizen
+                privacy, encryption, free speech, and blockchain technology to
+                empower the people at home so that the next generation of kids
+                can really <em>understand</em> privacy from their hands defining
+                true freedom and controlled exposure.
               </p>
             </div>
           </div>
@@ -159,10 +195,18 @@ export default function About() {
               <SocialLink href="#" icon={InstagramIcon} className="mt-4">
                 Follow on Instagram
               </SocialLink>
-              <SocialLink href="https://github.com/HeavenlyEntity" icon={GitHubIcon} className="mt-4">
+              <SocialLink
+                href="https://github.com/HeavenlyEntity"
+                icon={GitHubIcon}
+                className="mt-4"
+              >
                 Follow on GitHub
               </SocialLink>
-              <SocialLink href="https://www.linkedin.com/in/alec-mingione-90bb63aa/" icon={LinkedInIcon} className="mt-4">
+              <SocialLink
+                href="https://www.linkedin.com/in/alec-mingione-90bb63aa/"
+                icon={LinkedInIcon}
+                className="mt-4"
+              >
                 Follow on LinkedIn
               </SocialLink>
               <SocialLink
